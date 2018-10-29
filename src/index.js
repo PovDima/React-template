8607bb417a051003bba54705bf64d766e014e5ab
PovDima <povdima19@gmail.com>
2018-10-29 12:46:40 +0200

    fix commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import { Provider } from 'react-redux'
-import store from './reducer/store'
+import App from './components/App'
 ReactDOM.render(
-  <Provider store={store}>
-  <App />
-  </Provider>,
+  <App />,
  document.getElementById('root'));
